2fd2426349ee9d523e6c2a9473b7f1681bf687da
VictorRoe <victorroe819@gmail.com>
2025-02-03 15:11:14 -0500

    Delete another anotation lombok

--- weatherApp/src/main/java/com/victorroe/weatherApp/Controller/CurrentController.java
+++ weatherApp/src/main/java/com/victorroe/weatherApp/Controller/CurrentController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequiredArgsConstructor
 public class CurrentController {
 
     private final WeatherService weatherService;
