8b9f8259d55bf25d7e5184d5190c3be408af28b7
VictorRoe <victorroe819@gmail.com>
2025-02-11 15:34:18 -0500

    fix problem with suspense

--- weather-app/src/app/weather/layout.tsx
+++ weather-app/src/app/weather/layout.tsx
@@ -1,13 +1,16 @@
 import { Footer } from "../Components/Footer";
 import { Headers } from "../Components/Headers";
+import { Suspense } from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="flex flex-col min-h-screen bg-gradient-to-b from-[#1E1E1E] to-[#FF5E71] from-20% ">
         <Headers />
-        <main className="flex-grow flex items-center justify-center">
-          {children}
-        </main>
+        <Suspense fallback={<p className="text-white">Cargando clima</p>}>
+          <main className="flex-grow flex items-center justify-center">
+            {children}
+          </main>
+        </Suspense>
       </div>
       <Footer />
     </>
